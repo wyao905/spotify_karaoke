ae5f0e72e63c580922b9fbd70a7bc80ed7f26e16
wyao905 <warrenyca905@gmail.com>
2020-06-02 16:42:49 -0400

    added reducer file, modified import in index.js file for reducer

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import App from './App';
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
-// need to import reducer file
+import reducer from './reducers/reducer'
 
-const store = createStore(/*reducer file here*/, applyMiddleware(thunk))
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
