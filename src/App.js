bb61d7c87521c9b15b99fa831f85e7ecee28d6bf
wyao905 <warrenyca905@gmail.com>
2020-06-02 16:54:11 -0400

    added a few routes in App.js, imported BrowserRouter, Switch, and Route components from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-don"
 import './App.css';
 
 class App extends Component {
   render() {
     return(
       <Router>
-        
+        <Switch>
+          <Route path="/signup">
+
+          </Route>
+          <Route path="/user">
+
+          </Route>
+          <Route path="/">
+            
+          </Route>
+        </Switch>
       </Router>
     )
   }
